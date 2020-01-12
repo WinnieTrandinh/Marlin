735fc1fb7109d430d8715ef1380d4a0510679967
Theo <theodonkulus@gmail.com>
2020-01-12 13:05:38 -0500

    Jan 12 PID tune for WH3 hotend

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -380,9 +380,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Wanhao Duplicator i3
-  #define DEFAULT_Kp 29.60
-  #define DEFAULT_Ki 2.54
-  #define DEFAULT_Kd 86.09
+  #define DEFAULT_Kp 17,86
+  #define DEFAULT_Ki 0.84
+  #define DEFAULT_Kd 95.45
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
