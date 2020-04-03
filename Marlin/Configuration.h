f1b87abcf5208851436a4191d2aef0dd8f5912b0
Theo <theodonkulus@gmail.com>
2020-04-02 23:32:42 -0400

    Update PID values with new hotend thermistor settings for 06mm brass stock nozzle
    0.6mm brass using stock nozzle PI tune @220C

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -380,9 +380,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Wanhao Duplicator i3
-  #define DEFAULT_Kp 16.06
-  #define DEFAULT_Ki 0.73
-  #define DEFAULT_Kd 88.46
+  #define DEFAULT_Kp 25.83
+  #define DEFAULT_Ki 1.58
+  #define DEFAULT_Kd 105.26
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
